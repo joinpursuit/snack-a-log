79d44214fa677b3fb5cc2eb5f7323a76bcb9a996
ziqian pan <ziqianpan1998@gmail.com>
2022-08-16 20:07:54 -0400

    got all data

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,30 @@
 // DEPENDENCIES
 
+const cors = require("cors");
 const express = require("express");
 
 // CONFIGURATION
 const app = express();
 
 // MIDDLEWARE
+app.use(cors());
+app.use(express.json());
+
+
+const snackController = require("./controllers/snackController");
 
 // ROUTES
+app.get("/", (req, res) => {
+    res.send("Welcome To Snack-o-Log");
+  });
+
+app.use("/snacks", snackController);
+
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found");
+});
+
+
 
 // EXPORT
 module.exports = app;
--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -1,3 +1,15 @@
 const db = require("../db/dbConfig.js");
 
-module.exports = {};
+
+const getAllSnacks = async () => {
+    try {
+     const allSnacks = await db.any("SELECT * FROM snacks");
+     return allSnacks;
+    } catch(err) {
+      return err
+    } 
+  }
+
+
+
+module.exports = { getAllSnacks };
