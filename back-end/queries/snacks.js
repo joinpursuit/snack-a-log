ed0e4f5f5e451377221fa0c14f69b565a7ef252b
Denisse Maldonado <denissemaldonado@Denisses-MacBook-Air.local>
2022-08-16 20:24:34 -0400

    added controllers

--- back-end/queries/snacks.js
+++ back-end/queries/snacks.js
@@ -1,3 +1,27 @@
 const db = require("../db/dbConfig.js");
 
-module.exports = {};
+const getAllSnacks = async () => {
+    try {
+        const allSnacks = await db.any("SELECT * FROM snacks");
+        return allSnacks;
+    } catch (err) {
+        return err;
+    }
+}
+
+const getSnack = async (id) => {
+    try {
+        const oneSnack = await db.one("SELECT * FROM snacks WHERE id=$1", id);
+        return oneSnack;
+    } catch (err) {
+        return err;
+    }
+}
+
+
+
+
+module.exports = {
+    getAllSnacks,
+    getSnack
+};
