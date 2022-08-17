97f1c88a872791426e26ed580617a5d1f7c669ca
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-08-16 21:25:54 -0400

    adds basic server and CRUD

---
+++ back-end/controllers/snackController.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const snacks = express.Router();
+
+const { getAllSnacks, getSnack, createSnack, updateSnack, deleteSnack } = require("../queries/snacks");
+
+snacks.get("/", async (req, res) => {
+    const allSnacks = await getAllSnacks();
+    if(allSnacks[0]){
+        res.status(200).json({payload: allSnacks, success: true});
+    }else{
+        res.status(500).json({error: "server error"});
+    };
+});
+
+snacks.get("/:id", async (req, res) => {
+    const { id } = req.params;
+    const snack = await getSnack(id);
+    if(snack.id){
+        res.json({payload: snack, success: true});
+    }else{
+        res.status(404).json({payload: "not found", success: false});
+    };
+});
+
+snacks.post("/", async (req, res) => {
+    try{
+        const snack = await createSnack(req.body);
+        res.json({payload: snack, success: true});
+    }catch(err){
+        return err;
+    };
+});
+
+snacks.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedSnack = await updateSnack(req.body, id);
+    if(updatedSnack.id){
+        res.status(200).json({payload: updatedSnack, success: true});
+    }else{
+        res.status(400).json({payload: "bad request", success: false});
+    };
+});
+
+snacks.delete("/:id", async (req, res) => {
+    const { id } = req.params;
+    const deletedSnack = await deleteSnack(id);
+    if(deletedSnack.id){
+        res.status(200).json({payload: deletedSnack, success: true});
+    }else{
+        res.status(404).json({payload: "not found", success: false});
+    }
+})
+
+module.exports = snacks;
--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,4 +3,16 @@ CREATE DATABASE snack_a_log;
 
 \c snack_a_log; 
 
+CREATE TABLE snacks (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    fiber INTEGER,
+    protein INTEGER,
+    added_sugar INTEGER,
+    is_healthy BOOLEAN,
+    image TEXT
+);
+
+
+
 
