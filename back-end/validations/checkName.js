d4aff74c3998ed50e1e3ade9296623ec2c84cd70
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-08-17 20:27:05 -0400

    backend passes all tests

--- back-end/validations/checkName.js
+++ back-end/validations/checkName.js
@@ -2,7 +2,7 @@ const checkName = (snack) => {
   if (!snack.image) {
     snack.image =
       'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image';
-  }
+  };
 
   let splitName = snack.name.split(' ');
   let adjustedName = splitName
