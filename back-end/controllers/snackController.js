c1516be2c1754050b5fb217545af7285f383527e
Jimmy Ayivor <jimmyayivor@gmail.com>
2022-08-19 15:11:38 -0400

    bug fix

--- back-end/controllers/snackController.js
+++ back-end/controllers/snackController.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const snacks = express.Router();
-const { getAllSnacks, getSnack, deleteSnack, createSnack } = require("../queries/snacks.js");
+const {
+  getAllSnacks,
+  getSnack,
+  deleteSnack,
+  createSnack,
+} = require("../queries/snacks.js");
 
-const {checkImage, checkName} = require('../validations/checkSnacks.js')
+const { checkImage, checkName } = require("../validations/checkSnacks.js");
+
 snacks.get("/", async (req, res) => {
   const allSnacks = await getAllSnacks();
   if (allSnacks[0]) {
@@ -26,14 +32,15 @@ snacks.get("/:id", async (req, res) => {
 snacks.delete("/:id", async (req, res) => {
   const { id } = req.params;
   const deletedSnack = await deleteSnack(id);
-  if (deleteSnack.id) {
+
+  if (deletedSnack.id) {
     res.status(200).json({ payload: deletedSnack, success: true });
   } else {
     res.status(404).json({ payload: "snack not found", success: false });
   }
 });
 
-snacks.post("/",checkImage, checkName, async (req, res) => {
+snacks.post("/", checkImage, checkName, async (req, res) => {
   try {
     const snack = await createSnack(req.body);
     res.status(200).json({ payload: snack, success: true });
