1fc0a445b22ded575992eca798bbe6d00e08472b
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-08-18 22:06:00 -0400

    adds new form functionality and styling for home, new and error

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,19 +7,22 @@ import Snacks from "./Components/Snacks";
 import SnackDetails from "./Components/SnackDetails";
 import NewSnackForm from "./Components/NewSnackForm";
 
+//PAGES
+import Home from "./Pages/Home";
+import Error from "./Pages/Error";
+
 const App = () => {
   return (
     <div className="App">
       <Router>
         <NavBar />
-          <h1>Hello, world!</h1>
         <Routes>
-          <Route path="/" />
+          <Route path="/" element={<Home />} />
           <Route path="/snacks" element={<Snacks />} />
           <Route path="/snacks/new" element={<NewSnackForm />} />
           <Route path="/snacks/:id" element={<SnackDetails />} />
           <Route path="/snacks/:id/edit" element="" />
-          <Route path="*" element="" />
+          <Route path="*" element={<Error />} />
         </Routes>
       </Router>
     </div>
