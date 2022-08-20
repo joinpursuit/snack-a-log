9881865cd5984f25cd2d17476568d90975bed656
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-08-20 18:13:03 -0400

    SHOW PAGE- CSS

--- front-end/src/Components/SnackDetails.js
+++ front-end/src/Components/SnackDetails.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-
+import "./SnackDetails.css";
 
 function SnackDetails() {
   const [snack, setSnack] = useState({});
@@ -9,7 +9,6 @@ function SnackDetails() {
   let navigate = useNavigate();
 
   const API = process.env.REACT_APP_API_URL;
-
 
   useEffect(() => {
     axios
@@ -30,7 +29,7 @@ function SnackDetails() {
     axios
       .delete(`${API}snacks/${index}`)
       .then((response) => navigate(`/snacks`))
-      .catch((c) => console.error("catch", c));
+      .catch((error) => console.error("catch", error));
   };
 
   const handleDelete = () => {
@@ -46,16 +45,24 @@ function SnackDetails() {
   //   .catch((err) => console.log(err))
 
   // }
-  console.log("Line 49: ",snack.image)
+
   return (
-    <div>
+    <div className="snack-details">
       {/* is_healthy heart */}
-      <h3>Snack name: {snack.name} </h3>
-      <img src={snack.image}/>
-      
-      <h3>Snack fiber :{snack.fiber}</h3>
-      <h3>Snack protein: {snack.protein}</h3>
-      <h3>Snack added sugar {snack.added_sugar}</h3>
+      <div className="snack-detail">
+
+      <section className ='snack-visual'>
+
+      <h3 className='snack'>{snack.name} </h3>
+      <img src={snack.image} alt={snack.name} />
+      </section>
+      <section className="snack-info">
+      {snack.fiber > 1 ? <h3>Snack Fiber: <br></br><span className='result'>{snack.fiber} grams</span></h3> : <h3>Snack Fiber: <br></br><span className='result'>{snack.fiber} gram</span></h3>}
+      {snack.protein > 1 ? <h3>Snack Protein:<br></br> <span className='result'>{snack.protein} grams</span></h3> : <h3>Snack Protein: <br></br><span className='result'>{snack.protein} gram</span></h3>}
+      {snack.added_sugar > 1 ? <h3>Snack Added Sugar: <br></br> <span className ='result'>{snack.added_sugar} grams</span></h3> : <h3>Snack Added Sugar: <br></br><span className='result'>{snack.added_sugar} gram</span></h3>}
+      </section>
+      </div>
+
       <Link to={`/snacks`}>
         <button className="back-button">Back</button>
       </Link>
