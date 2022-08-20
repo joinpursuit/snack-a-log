2b34137da62e473ebe1fb2ba5cca35c1b70c9b38
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-08-20 11:59:43 -0400

    Snacks Css

--- front-end/src/Components/Snack.js
+++ front-end/src/Components/Snack.js
@@ -2,16 +2,13 @@ import { Link } from "react-router-dom";
 
 function Snack({ snack, index }) {
   return (
-    <div>
-        <Link to={`/snacks/${index}`}>
+    <div className ='snacks'>
+      <Link to={`/snacks/${index}`}>
         <>
-      <img src={snack.image} alt={snack.name}></img>
-
-          <p>{snack.name}</p> 
+          <img src={snack.image} alt={snack.name}></img>
+          <p>{snack.name}</p>
         </>
-          </Link>
-    
-      {/* <li>{transaction.date}<br></br>{transaction.item_name}<br></br>{transaction.amount}</li> */}
+      </Link>
     </div>
   );
 }
--- front-end/src/Components/Snacks.js
+++ front-end/src/Components/Snacks.js
@@ -1,42 +1,32 @@
 import { useState, useEffect } from "react";
 
-import Snack from "../Components/Snack.js"
+import Snack from "../Components/Snack.js";
+import "./Snacks.css";
 
 import axios from "axios";
 
-
 const API = process.env.REACT_APP_API_URL;
 
-
-
 function Snacks() {
+  const [snacks, setSnacks] = useState([]);
 
-    const [snacks, setSnacks] = useState([]);
-
-    
-
-    useEffect(() => {
-
-        axios
-          .get(`${API}/snacks`)
-          .then((res) => setSnacks(res.data))
-          .catch((err) => console.error(err));
-      }, []);
-console.log(snacks)
+  useEffect(() => {
+    axios
+      .get(`${API}/snacks`)
+      .then((res) => setSnacks(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+  console.log(snacks);
 
   return (
     <div>
-    <section>
-    {snacks.map((snack, index) => {
-      return(
-        
-        <Snack  index={index} snack={snack}/>
-        
-      )
-    })}
-    </section>
+      <section className='snacks-index'>
+        {snacks.map((snack, index) => {
+          return <Snack index={index} snack={snack} />;
+        })}
+      </section>
     </div>
-  )
+  );
 }
 
-export default Snacks
+export default Snacks;
