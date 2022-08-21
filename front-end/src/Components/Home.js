614516f7f3ec3f569e9da3a1be3b626c3d00be03
Kalilah Clarke <kalilah.clarke@gmail.com>
2022-08-21 11:43:06 -0400

    Home.Css

--- front-end/src/Components/Home.js
+++ front-end/src/Components/Home.js
@@ -4,9 +4,12 @@ import image from './Images/Image3.webp'
 
 function Home() {
   return (
-    <div>
+    <div className ="home">
+      <h2>Welcome to</h2> 
       <img className = 'vending-machine' src ={image} alt ='velding-machine'></img>
-        <h2>Welcome to the Vending Machine</h2>
+      <img className = 'vending-machine' src ={image} alt ='velding-machine'></img>
+      <img className = 'vending-machine' src ={image} alt ='velding-machine'></img>
+      <h2>The Vending Machine App.</h2>
     </div>
   )
 }
