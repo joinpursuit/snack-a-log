f4330132992af9a0f1e681bab1503d14a06db15a
Khaleed Uddin <tashrif@Khaleeds-MacBook-Air-2.local>
2022-08-21 16:07:26 -0400

    updated homepage

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -5,6 +5,7 @@ function Home () {
         <div className="HomePage">
            <h2>Welcome to the Best Snackin' App!!!!!!</h2> 
            <img src="https://www.mashed.com/img/gallery/the-cookie-monsters-cookies-arent-what-you-think/intro-1642500635.webp"></img>
+           <h6>By Khaleed Uddin and Soma Majumder</h6>
             </div> 
     )
 }
