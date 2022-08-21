d449d4a1a87edb5bd1d89c07dea8cd43b6feed9d
Khaleed Uddin <tashrif@Khaleeds-MacBook-Air-2.local>
2022-08-21 16:17:27 -0400

    added alt for img so netlify doesnt kill us

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -4,7 +4,7 @@ function Home () {
     return (
         <div className="HomePage">
            <h2>Welcome to the Best Snackin' App!!!!!!</h2> 
-           <img src="https://www.mashed.com/img/gallery/the-cookie-monsters-cookies-arent-what-you-think/intro-1642500635.webp"></img>
+           <img src="https://www.mashed.com/img/gallery/the-cookie-monsters-cookies-arent-what-you-think/intro-1642500635.webp" alt="cookie monster"></img>
            <h6>By Khaleed Uddin and Soma Majumder</h6>
             </div> 
     )
