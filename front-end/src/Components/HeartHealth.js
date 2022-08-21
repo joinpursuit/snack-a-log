6ef937138ee4a0fa6b6b092906b839039ab10f96
Soma Mahajaya <sokairavi@gmail.com>
2022-08-21 11:56:55 -0400

    Soma - Pages, HeartHealth, SnackDetails, Snack, Snacks

--- front-end/src/Components/HeartHealth.js
+++ front-end/src/Components/HeartHealth.js
@@ -1,21 +1,22 @@
+import React from "react";
 import heartSolid from "../assets/heart-solid.png";
 import heartOutline from "../assets/heart-regular.png";
 
-const HeartHealth({ snackHealth }) => {
+const HeartHealth = ({ snackHealth }) => {
 
-  if(snackHealth.is_healthy){
-    return (
-        <aside>
-          <img src={heartSolid} alt="healthy food" />
-        </aside>
-      )
-  }else if(!snackHealth.is_healthy){
-    return (
-        <aside>
-          <img src={heartOutline} alt="unhealthy food" />
-        </aside>
-      )
-  }
+    if(snackHealth.is_healthy){
+      return (
+          <aside>
+            <img src={heartSolid} alt="healthy food" />
+          </aside>
+        )
+    }else if(!snackHealth.is_healthy){
+      return (
+          <aside>
+            <img src={heartOutline} alt="unhealthy food" />
+          </aside>
+        )
+    }
 }
 
 
