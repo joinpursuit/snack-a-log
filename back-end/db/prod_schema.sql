3e9048e4a3feecb53d3100249fffc6228c4dc0af
Jorge Carrera <JCarrera994@gmail.com>
2022-08-22 22:04:37 -0400

    heroku deployment

---
+++ back-end/db/prod_schema.sql
@@ -0,0 +1,13 @@
+
+
+CREATE TABLE snacks (
+  id SERIAL PRIMARY KEY, 
+  name TEXT NOT NULL,
+  fiber INTEGER,
+  protein INTEGER,
+  added_sugar INTEGER,
+  is_healthy BOOLEAN,
+  image TEXT
+); 
+
+
