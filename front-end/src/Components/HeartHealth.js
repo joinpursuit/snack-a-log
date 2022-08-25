a5750b0cd30a346f7ac1ef71b3f3aa2b0274513b
Kenyetta Griffin <kenyettagriffin@pursuit.org>
2022-08-24 22:29:39 -0400

    modal

--- front-end/src/Components/HeartHealth.js
+++ front-end/src/Components/HeartHealth.js
@@ -27,7 +27,7 @@ function HeartHealth({ snackHealth }) {
           <Modal.Title>Health Status</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-        {snackHealth.fiber > 5 || snackHealth.protein > 5 || snackHealth.added_sugar < 5   ? 'This snack is UNHEALTHY' : 'This snack is HEALTHY'}
+        {snackHealth.fiber > 5 || snackHealth.protein > 5 && snackHealth.added_sugar < 5   ? 'This snack is UNHEALTHY' : 'This snack is HEALTHY'}
         </Modal.Body>
         <Modal.Footer>
           <button onClick={hideModal}>THANK YOU</button>
